34daf17f5b7669d1e1fdfb7a51866a2c4a4c29b5
Miguel Lucas <lucasmiguel646@gmail.com>
2024-07-20 01:44:03 -0300

    Solução do Exemplo sem o uso de interface

---
+++ questoes/src/Seção18/ExProblem/model/services/RentalService.java
@@ -0,0 +1,41 @@
+package Seção18.ExProblem.model.services;
+
+import Seção18.ExProblem.model.entities.CarRental;
+import Seção18.ExProblem.model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental){
+
+        //Calculo entre as datas
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+
+        double basicPayment;
+        if(hours <= 12){
+            basicPayment = pricePerHour * Math.ceil(hours);//Math.ceil irá arredondar as horas para cima
+        }
+        else {
+            basicPayment = pricePerDay * Math.ceil(hours/24);
+
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+
+    }
+
+}
